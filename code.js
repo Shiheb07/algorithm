396a0c7ae1d8213583264863b0a20a45788bc7b4
Shiheb07 <116961351+Shiheb07@users.noreply.github.com>
2023-02-25 17:51:46 +0100

    task 1 + task 2

--- code.js
+++ code.js
@@ -1,35 +1,3 @@
-// // Function to calculate dot product
-// function dot_product(v1, v2) {
-//     let ps = 0;
-//     for (let i = 0; i < v1.length; i++) {
-//       ps += v1[i] * v2[i];
-//     }
-//     return ps;
-//   }
-  
-//   // Function to determine if two vectors are orthogonal
-//   function are_orthogonal(v1, v2) {
-//     return dot_product(v1, v2) === 0;
-//   }
-  
-//   // Test cases
-//   let v1 = [1, 2, 3];
-//   let v2 = [4, 5, 6];
-//   let v3 = [-1, 2, -1];
-//   let v4 = [2, -4, 2];
-  
-//   console.log(dot_product(v1, v2)); // Output: 32
-//   console.log(dot_product(v3, v4)); // Output: 0
-  
-//   console.log(are_orthogonal(v1, v2)); // Output: false
-//   console.log(are_orthogonal(v3, v4)); // Output: true
-  
-
-//_______________________________##________________________________//
-
-
-//Task-2:
-
 function insertionSort(arr) {
   // Loop through each element of the array
   for (let i = 1; i < arr.length; i++) {
@@ -48,6 +16,4 @@ function insertionSort(arr) {
   
   return arr;
 }
-console.log(insertionSort([3, 5, 1, 8, 2, 9, 4])) 
-
-
+console.log(insertionSort([3, 5, 1, 8, 2, 9, 4]))
